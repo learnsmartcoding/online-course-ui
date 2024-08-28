213bb68ec71a0fb01416713dfeb972dcc5f6e0ab
Learn Smart Coding <learnsmartcoding@users.noreply.github.com>
2024-08-28 00:16:08 -0400

    Added code logic for azure ad b2c

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,13 +11,135 @@ import { SafePipe } from './pipes/safe.pipe';
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
+  withFetch,
   withInterceptorsFromDi,
 } from '@angular/common/http';
 import { HttpRequestInterceptor } from './services/spinner-interceptor';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideNoopAnimations } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
+import {
+  IPublicClientApplication,
+  PublicClientApplication,
+  InteractionType,
+  BrowserCacheLocation,
+  LogLevel,
+} from '@azure/msal-browser';
+import {
+  MsalInterceptor,
+  MSAL_INSTANCE,
+  MsalInterceptorConfiguration,
+  MsalGuardConfiguration,
+  MSAL_GUARD_CONFIG,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService,
+  ProtectedResourceScopes,
+} from '@azure/msal-angular';
+import { environment } from '../environments/environment';
+
+
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+export const b2cPolicies = {
+  names: {
+    signUpSignIn: 'B2C_1_susi',
+    resetPassword: 'B2C_1_password_reset',
+    editProfile: 'B2C_1_profile_edit',
+  },
+  authorities: {
+    signUpSignIn: {
+      authority:
+        'https://smartlearnbykarthik.b2clogin.com/smartlearnbykarthik.onmicrosoft.com/B2C_1_susi',
+    },
+    resetPassword: {
+      authority:
+        'https://smartlearnbykarthik.b2clogin.com/smartlearnbykarthik.onmicrosoft.com/B2C_1_password_reset',
+    },
+    editProfile: {
+      authority:
+        'https://smartlearnbykarthik.b2clogin.com/smartlearnbykarthik.onmicrosoft.com/B2C_1_profile_edit',
+    },
+  },
+  authorityDomain: 'smartlearnbykarthik.b2clogin.com',
+};
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.adb2cConfig.clientId,
+      authority: b2cPolicies.authorities.signUpSignIn.authority, //environment.msalConfig.auth.authority,
+      knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
+      redirectUri: '/',
+      postLogoutRedirectUri: '/',
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false,
+      },
+    },
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, 
+  //Array<string>
+  Array<string | ProtectedResourceScopes> | null
+  >();
+  //have this set if more microservice used or requires different scope for different controllers
+  // protectedResourceMap.set(
+  //   environment.adb2cConfig.apiEndpointUrl, // This is for all controllers
+  //   environment.adb2cConfig.scopeUrls
+  // );
+
+  protectedResourceMap.set(environment.adb2cConfig.apiEndpointUrl, [
+    // {
+    //   httpMethod: 'GET',
+    //   scopes: [...environment.adb2cConfig.scopeUrls]
+    // },
+    {
+      httpMethod: 'POST',
+      scopes: [...environment.adb2cConfig.scopeUrls]
+    },
+    {
+      httpMethod: 'PUT',
+      scopes: [...environment.adb2cConfig.scopeUrls]
+    },
+    {
+      httpMethod: 'DELETE',
+      scopes: [...environment.adb2cConfig.scopeUrls]
+    },
+    {
+      httpMethod: 'PATCH',
+      scopes: [...environment.adb2cConfig.scopeUrls]
+    },
+  ]);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap,
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: [...environment.adb2cConfig.scopeUrls],
+    },
+    loginFailedRoute: '/login-failed',
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -41,5 +163,27 @@ export const appConfig: ApplicationConfig = {
       useClass: HttpRequestInterceptor,
       multi: true,
     },
+    provideNoopAnimations(),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,20 @@ import { AboutComponent } from './components/core/about/about.component';
 import { ContactUsComponent } from './components/core/contact-us/contact-us.component';
 import { PlansAndPricingComponent } from './components/plans-and-pricing/plans-and-pricing.component';
 import { CourseDetailsComponent } from './components/course/course-details/course-details.component';
+import { ViewClaimsComponent } from './components/core/view-claims/view-claims.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'course/category', component: CategoryComponent },
   { path: 'course/category/:categoryId', component: CourseByCategoryComponent },
-  { path: 'course/session-details/:courseId', component: CourseDetailsComponent },
+  {
+    path: 'course/session-details/:courseId',
+    component: CourseDetailsComponent,
+  },
   { path: 'about-us', component: AboutComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'plans-and-price', component: PlansAndPricingComponent },
+  { path: 'claims', component: ViewClaimsComponent },
+  { path: '**', redirectTo: 'home' },
 ];
